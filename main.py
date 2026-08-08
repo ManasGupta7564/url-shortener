6f696dd8bd84d9441d8e15afaba81df9c33ad5bf
Manas Gupta <manas.gupta0704@gmail.com>
2026-08-08 12:45:07 +0530

    Add URL shortening endpoint

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class URLRequest(BaseModel):
+    url: str
 
 
 @app.get("/")
 def home():
     return {"message": "URL Shortener is running!"}
+
+
+@app.post("/shorten")
+def shorten_url(request: URLRequest):
+    return {
+        "short_url": "abc123"
+    }
